bf926feb8f9dbdc3c28b5afe078a21d1487f6cc3
Akryos <Akryos@users.noreply.github.com>
2023-03-27 00:41:44 +0200

    fix interval check and changing image displayed

--- scripts/movement.js
+++ scripts/movement.js
@@ -1,6 +1,4 @@
 let player;
-let playerCoordinateX = 0;
-let playerCoordinateY = 0;
 let tileSize = 32;
 let movementSpeed = 100; //smaller number equals faster animations
 var moveInterval;
@@ -10,7 +8,7 @@ let framePixelDistance = tileSize / 8;
 
 window.addEventListener('load', function () {
     player = this.document.getElementById('player');
-    player.style.top = "0px";
+    player.style.top = "0px"; //needed because otherwise it is an empty string
     player.style.left = "0px";
 
     window.addEventListener("keydown", performPlayerMovement);
@@ -18,49 +16,70 @@ window.addEventListener('load', function () {
 
 
 
-
-
-
 function performPlayerMovement(e) {
-    switch(e.keyCode) {
+    let direction = getMovementDirection(e.keyCode);
+
+    if(direction && !moveInterval) {
+        moveInterval = setInterval(function(){
+
+            switch(direction) {
+                case 'left':
+                    player.style.left = (parseInt(player.style.left, 10) - framePixelDistance) + "px";
+                    break;
+                case 'up':
+                    player.style.top = (parseInt(player.style.top, 10) - framePixelDistance) + "px";
+                    break;
+                case 'right':
+                    player.style.left = (parseInt(player.style.left, 10) + framePixelDistance) + "px";
+                    break;
+                case 'down':
+                    player.style.top = (parseInt(player.style.top, 10) + framePixelDistance) + "px";
+                    break;
+                default:
+                    break;
+            }
+
+            currentPlayerPostureNumber = (currentPlayerPostureNumber == 4) ? 1 : (++currentPlayerPostureNumber);
+            player.src = `./assets/player/${direction}_${currentPlayerPostureNumber}.png`;
+
+            loopCounter += framePixelDistance;
+
+            if(loopCounter == tileSize) {
+                loopCounter = 0;
+                moveInterval = clearInterval(moveInterval);
+            }
+        }, movementSpeed);
+    }
+}
+
+function getMovementDirection(eKeyCode) {
+    let direction;
+
+    switch(eKeyCode) {
         case 37: //left arrow
         case 65: //a key
+            direction = 'left';
             break;
 
         case 38: //up arrow
         case 87: //w key
+            direction = 'up';
             break;
 
         case 39: //right arrow
         case 68: //d key
+            direction = 'right';
             break;
 
         case 40: //down arrow
         case 83: //s key
-
-        if(!moveInterval) {
-            moveInterval = setInterval(function(){
-                player.style.top = parseInt(player.style.top, 10) + framePixelDistance + "px";
-
-                currentPlayerPostureNumber = (currentPlayerPostureNumber == 4) ? 1 : (++currentPlayerPostureNumber);
-                player.src = "./assets/player/down_" + currentPlayerPostureNumber + ".png";
-
-                loopCounter += framePixelDistance;
-
-                if(loopCounter == tileSize) {
-                    loopCounter = 0;
-                    clearInterval(moveInterval);
-                }
-            }, movementSpeed);
-        }
-
-
-
-
+            direction = 'down';
             break;
 
         default:
-            // console.log(e.keyCode);
+            return false;
             break;
     }
+
+    return direction;
 }
